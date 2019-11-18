7081d3699c15e21c3a601e99a9eef6defaf1d8b2
Sofian Eon <eon.sofian@hotmail.fr>
2019-11-18 21:19:32 +0100

    Correction d'une faue d'orthographe dans le controlleur

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -7,13 +7,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProstagesController extends AbstractController
 {
-    /**
-     * @Route("/prostages", name="prostages")
+     /**
+     * @Route("/", name="prostage-accueil")
      */
     public function index()
     {
-        return $this->render('prostages/index.html.twig', [
-            'controller_name' => 'ProstagesController',
-        ]);
+        return $this->render('prostages/index.html.twig');
+    }
+
+    /**
+     * @Route("/entreprises", name="prostage-entreprises")
+     */
+    public function afficherEntreprises()
+    {
+        return $this->render('prostages/affichageEntreprises.html.twig');
+    }
+
+    /**
+     * @Route("/formations", name="prostage-formations")
+     */
+    public function afficherFormations()
+    {
+        return $this->render('prostages/affichageFormations.html.twig');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="prostage-stage")
+     */
+    public function afficherStages($id)
+    {
+        return $this->render('prostages/affichageStages.html.twig', ['idStage' => $id]);
     }
 }
